be5c4e78b09eccb0a9e0a63317fd55497587d4fd
SuperArnold <ji394vul35520@hotmail.com>
2021-04-23 01:37:55 +0800

    新增時間計算時間的功能

--- 2.Word_format/cmd/root.go
+++ 2.Word_format/cmd/root.go
@@ -14,4 +14,9 @@ func init() {
 	rootCmd.AddCommand(wordCmd)
 	rootCmd.AddCommand(timeCmd)
 	rootCmd.AddCommand(nowTimeCmd)
+	rootCmd.AddCommand(calculateTimeCmd)
+
+	calculateTimeCmd.Flags().StringVarP(&calculateTime, "calculate", "c", "", "需要計算的時間")
+	calculateTimeCmd.Flags().StringVarP(&duration, "duration", "d", "", "持續時間")
+
 }
--- 2.Word_format/cmd/time.go
+++ 2.Word_format/cmd/time.go
@@ -3,6 +3,9 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/SuperArnold/GO_tools/2.Word_format/internal/timer"
 
@@ -27,6 +30,41 @@ var nowTimeCmd = &cobra.Command{
 	Long:  "取得現在時間",
 	Run: func(cmd *cobra.Command, args []string) {
 		nowTime := timer.GetNowTime()
-		log.Printf("現在時間為 %s, %d", nowTime.Format("2021-01-01 00:00:00"), nowTime.Unix())
+		log.Printf("現在時間為 %s, %d", nowTime.Format("2021-01-0100:00:00"), nowTime.Unix())
+	},
+}
+
+var calculateTimeCmd = &cobra.Command{
+	Use:   "cal",
+	Short: "計算時間",
+	Long:  "計算所需時間",
+	Run: func(cmd *cobra.Command, args []string) {
+		var currentTimer time.Time
+		var layout = "2006-01-0215:04:05"
+
+		if calculateTime == "" {
+			currentTimer = timer.GetNowTime()
+		} else {
+			var err error
+			space := strings.Count(calculateTime, " ")
+			if space == 0 {
+				layout = "2006-01-02"
+			}
+			if space == 1 {
+				layout = "Jan 2, 2006 at 3:04pm "
+			}
+
+			currentTimer, err = time.Parse(layout, calculateTime)
+			if err != nil {
+				t, _ := strconv.Atoi(calculateTime)
+				currentTimer = time.Unix(int64(t), 0)
+			}
+		}
+		fmt.Println(calculateTime)
+		t, err := timer.GetCalculateTime(currentTimer, duration)
+		if err != nil {
+			log.Fatalf("timer.GetCalculateTime err: %v", err)
+		}
+		log.Printf("結果： %s, %d", t.Format(layout), t.Unix())
 	},
 }
